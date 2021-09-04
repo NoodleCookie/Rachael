17d79376a616b6f76a620b9a742b1b088ecbbf90
Izumi <1042631369@qq.com>
2021-09-04 13:21:36 +0800

    init projection

---
+++ src/main/java/com/thoughtworks/rachael/RachaelApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.rachael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RachaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/rachael/RachaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.rachael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RachaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
