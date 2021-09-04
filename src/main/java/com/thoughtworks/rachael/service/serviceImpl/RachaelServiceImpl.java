1e1201c58c23ed7a5973e042e451d61e9364b749
Izumi <1042631369@qq.com>
2021-09-04 15:03:49 +0800

    add RachaelService

---
+++ src/main/java/com/thoughtworks/rachael/service/serviceImpl/RachaelServiceImpl.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.rachael.service.serviceImpl;
+
+import com.thoughtworks.rachael.service.RachaelService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RachaelServiceImpl implements RachaelService {
+}
